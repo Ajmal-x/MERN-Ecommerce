0d14c3b6b587e4970a04f773f5aca70f5025da2b
Ajmal <elyasspgm@gmail.com>
2026-09-21 15:01:26 +0300

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,6 @@ app.get("/", (req, res) => {
 });
 
 // INFO: Start server
-app.listen(port, () => {
-  console.log(`Server is running on at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
